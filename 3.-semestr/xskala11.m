663d5ff3f20a5604dc8da3043078e3907b116d10
Dominik Skála <DominikSkala@seznam.cz>
2016-12-28 17:00:50 +0100

    Update xskala11.m

--- 3.-semestr/xskala11.m
+++ 3.-semestr/xskala11.m
@@ -1,3 +1,38 @@
+% 1) Nacteni signalu a zakladni prace nad nim
 [Signal, Fs] = audioread('xskala11.wav');
+
+%signal musi byt radkovy vektor, pomoci fce whos zjisteno, ze je sloupcovy, proto inverze na radkovy
 Signal = Signal';
-spectrum = fft(Signal, Fs/2);
+
+%zobrazime hodnoty vzorkovaci frekvence
+fprintf('Vzorkovaci frekvence signalu xskala11.wav je: %d\n', Fs);
+
+%delka signálu
+siglen = length(Signal);
+fprintf('Delka signalu xskala11.wav je: %d v S/s nebo %d sekunda\n', siglen, (siglen/Fs));
+
+%2) FFT a spektrum
+spectrum = fft(Signal);
+f = (0:(Fs-1))/Fs*Fs/2;
+plot(f,abs(spectrum));
+print('Spectrum','-dpng');
+
+%3) Hodnota spektra v maximu
+[~, maxindex] = max(abs(spectrum));
+max = maxindex/Fs * Fs;
+fprintf('Maximum modulu spektra ma: %d Hz\n', max);
+
+
+%4) Filtrace
+%implementujeme filtr za pomoci fce zplane, aby se nam lepe videlo, zda-li je filtr stabilni
+%definice vstupu, pro pozdeji pouziti
+a = [0.2289 0.4662];
+b = [0.2324 -0.4112 0.2324];
+zplane(b,a);
+print('IIR_Filtr','-dpng');
+%filtr stabilni neni, protoze | pol/y (je pouze jeden) | > 1, pro stabilitu je treba, aby | pol/y | < 1, vice v doku
+
+%5) 
+[h,w] = freqz(b,a,512);
+plot(w);
+print('IIR_Filtr_kmitocet', '-dpng');
